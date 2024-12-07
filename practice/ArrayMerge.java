97535b3c204b0dcb5f54eb26818ccc3378388717
ye-guo <aidjajd@163.com>
2024-12-07 16:44:24 +0800

    fix: 大写类名字母开头

--- practice/ArrayMerge.java
+++ practice/ArrayMerge.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class arrayMerge {
+public class ArrayMerge {
     public static void main(String[] args) {
         int[] nums1 = {1,2,3,0,0,0};
         int[] nums2 = {2,5,6};
